1bcf076b49a4a726cb010c90d2974996a3888b2a
Rebeca Baruch <rebecabaruch@outlook.com>
2021-12-20 21:16:19 -0300

    aprendendo o que são props
    reenderizando as tasks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Tasks from './components/Tasks';
 import './App.css';
 
 const App = () => {
 
-  const message = "Hello world!";
+  const [tasks, setTasks] = useState([
+    {
+      id: '1',
+      title: 'estudar programação',
+      completed: false,
+    },
 
+    {
+      id: '2',
+      title: 'Ler livro',
+      completed: true,
+    }
+  ]);
+  
   return(
-    <div className='container'>
-
-    </div>
+      <>
+          <div className='container'>
+            <Tasks tasks={tasks} />
+          </div>
+      </>
   )
-        
 }
 
 export default App;     
